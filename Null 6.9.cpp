d87fbf33389aff9c8207fac52438834a9a617a52
4D-04\koyo <a>
2026-06-09 15:52:38 +0900

    Null演習②ムーブ、所有権の転送までやった

--- Null 6.9.cpp
+++ Null 6.9.cpp
@@ -5,96 +5,117 @@
 #include <iostream>
 #include <memory>
 #include <assert.h>
-
-
+//--------------------------------------------------
+// 武器クラス（親クラス）
+//--------------------------------------------------
 class Weapon {
-
 public:
-
+	// virtual を付けることで
+	// 子クラス側で関数を上書きできる
 	virtual void Use() {
-
 		std::cout << "Weapon Attack!" << std::endl;
-
 	}
-
 };
-// 演習②　ムーブを用いた所有権の転送
+//--------------------------------------------------
+// 剣クラス（Weapon を継承）
+//--------------------------------------------------
 class Sword : public Weapon {
-
 public:
-
+	// override
+	// 「親クラスの関数を上書きしています」
+	// という意味
 	void Use() override {
-
 		std::cout << "Sword Slash!" << std::endl;
-
 	}
-
 };
-
+//--------------------------------------------------
+// プレイヤークラス
+//--------------------------------------------------
 class Player {
-
+	// shared_ptr
+	// 自動でメモリ管理をしてくれるスマートポインタ
 	std::shared_ptr<Weapon> weapon;
-
 public:
-
-	// ムーブで受け取る
-	Player(std::shared_ptr<Weapon> weapon1): weapon(std::move(weapon1))
-
+	//--------------------------------------------------
+	// コンストラクタ
+	//--------------------------------------------------
+	// weapon1 を受け取る
+	Player(std::shared_ptr<Weapon> weapon1)
+		// std::move を使うことで
+		// weapon1 の所有権を weapon に移動する
+		//
+		// コピーではないので
+		// 無駄な参照カウント増加を防げる
+		: weapon(std::move(weapon1))
 	{
-
 		std::cout << "Player Constructor" << std::endl;
-
+		//--------------------------------------------------
+		// move後の参照カウント確認
+		//--------------------------------------------------
+		// weapon 側
+		// 所有権を持っているので 1
 		std::cout << "weapon use_count : "
 			<< weapon.use_count()
 			<< std::endl;
-
+		// weapon1 側
+		// moveされたので nullptr 状態
+		// use_count は 0
 		std::cout << "weapon1 use_count : "
 			<< weapon1.use_count()
 			<< std::endl;
-
 	}
-
+	//--------------------------------------------------
+	// 攻撃関数
+	//--------------------------------------------------
 	void Attack() {
-
+		// weapon の Use() を実行
 		weapon->Use();
-
 	}
-
 };
-
 int main() {
-
+	//--------------------------------------------------
+	// Swordオブジェクト生成
+	//--------------------------------------------------
+	// make_shared を使うことで
+	// shared_ptr を安全に生成できる
 	auto sword = std::make_shared<Sword>();
-
+	//--------------------------------------------------
+	// move前の参照カウント確認
+	//--------------------------------------------------
 	std::cout << "Before move" << std::endl;
-
-	std::cout << "sword : "<< sword.use_count()<< std::endl;
-
-	// 所有権を移動
-	Player p1(std::move(sword));
-
-	std::cout << "After move" << std::endl;
-
+	// 現在 sword だけが所有しているので 1
 	std::cout << "sword : "
 		<< sword.use_count()
 		<< std::endl;
-
-
-
-	// move後は nullptr になる
+	//--------------------------------------------------
+	// 所有権を Player に移動
+	//--------------------------------------------------
+	// std::move を使うことで
+	// sword の所有権を p1 に渡す
+	Player p1(std::move(sword));
+	//--------------------------------------------------
+	// move後の確認
+	//--------------------------------------------------
+	std::cout << "After move" << std::endl;
+	// move後なので sword は nullptr
+	// use_count は 0
+	std::cout << "sword : "
+		<< sword.use_count()
+		<< std::endl;
+	//--------------------------------------------------
+	// sword が nullptr か確認
+	//--------------------------------------------------
+	// move後は中身が空になる
 	if (sword == nullptr) {
-
 		std::cout << "sword is nullptr" << std::endl;
-
 	}
-
+	//--------------------------------------------------
+	// プレイヤー攻撃
+	//--------------------------------------------------
+	// Player が所有している weapon を使用
 	p1.Attack();
-
 	return 0;
-
 }
-
-
 
 
 
